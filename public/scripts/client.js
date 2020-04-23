3ac6b6758e3b0acf6aaa74d61fed9f6f463a5024
Judith Huynh <judithuynh@gmail.com>
2020-04-23 02:02:46 +0000

    removed hard coded in tweets-container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,68 +1,29 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-const tweetObject = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
- 
-  
-
-
-
-const createTweetElement = function(tweetObject) {
+ const createTweetElement = function(tweetObject) {
+  console.log(tweetObject);
+  console.log(tweetObject.user);
+  console.log(tweetObject.user.avatars)
   let $tweet = $('<article>').addClass('tweet');
-  // $('#tweets-container').append($tweet)
-    const $header = $(`<header>`); 
-    // --------Header Elements-----------//
-    const $img = $('<img>').addClass('avatar');
-    $img.attr('src', tweetObject.user.avatars);
-    const $name = $(`<p>`).addClass('name');
-    $name.text(tweetObject.user.name).appendTo($header);
-    const $username = $(`<p>`).addClass(`username`);
-    $username.text(tweetObject.user.handle).appendTo($header);
-   /** add header to article */
-    $tweet.append($header);
-   // ---- Body of tweet ---//
-    const $userTweetDiv = $('<div>')
-    const $tweetContext = $('<p>').addClass('tweetContext')
-    $tweetContext.text(tweetObject.content.text).appendTo($userTweetDiv);
-    $tweet.append($userTweetDiv);
-    // ---- footer Elements ---- //
-    const $footer = $('<footer>');
-    const $dateP = $('<p>');
-    $dateP.text(tweetObject.created_at).appendTo($footer);
-    const $iconsDiv = $('<div>').addClass('icons');
-    $iconsDiv.appendTo($footer)
-    const $flagIcon = $('<i>').addClass('fas fa-flag');
-    $flagIcon.appendTo($iconsDiv)
-    const $retweetIcon = $('<i>').addClass('fas fa-retweet');
-    $retweetIcon.appendTo($iconsDiv)
-    const $heartIcon = $('<i>').addClass('fas fa-retweet');
-    $heartIcon.appendTo($iconsDiv)
-    $tweet.append($footer)
+  // --------Header Elements-----------//
+  const $header = $(`<header>`); 
+        $header.append($(`<img src = ${tweetObject.user.avatars}>`).addClass('avatar'));
+        $header.append($(`<p>`).addClass('name').text(tweetObject.user.name));
+        $header.append($(`<p>`).addClass(`username`).text(tweetObject.user.handle));
+  // ---- Body of tweet ---//
+  const $userTweetDiv = $('<div>')
+        $userTweetDiv.append($('<p>').addClass('tweetContext').text(tweetObject.content.text));
+    
+  // ---- Footer Elements ---- //
+  const $footer = $('<footer>');
+        $footer.append($('<p>').addClass('date').text(tweetObject.created_at));
+  const $iconsDiv = $('<div>').addClass('icons');
+        $iconsDiv.append($('<i>').addClass('fas fa-flag'));
+        $iconsDiv.append($('<i>').addClass('fas fa-retweet'));
+        $iconsDiv.append($('<i>').addClass('fas fa-heart'));
+  $footer.append($iconsDiv);
+  
+  $tweet.append($header); //header
+  $tweet.append($userTweetDiv); //body
+  $tweet.append($footer); //footer
   return $tweet;
 };
 
@@ -70,10 +31,37 @@ const createTweetElement = function(tweetObject) {
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
 const renderTweets = function(tweets) {
-  $.each(tweets, (tweetObject) => {
+  tweets.forEach((tweetObject) => {
     $('#tweets-container').append(createTweetElement(tweetObject));
   });
 }
-// const $tweet = createTweetElement(tweetData);
+const loadTweets = function () {
+  $.ajax( {
+      url: '/tweets',
+      type: 'GET',
+      success: response => renderTweets(response)
+  })
+};
 
-renderTweets(tweetObject)
+
+$(document).ready(function() {
+  loadTweets();
+
+  $('#tweetForm').submit(function (event) {
+    event.preventDefault();
+    
+    //information from the server 
+    let text = $('#tweet-text').val()
+    console.log(text);
+    $.ajax( {
+        url: `/tweets`,
+        method: "POST",
+        data: { text }
+      })
+      .then((tweet) => {createTweetElement(tweet).appendTo('#tweets-container')})
+  });
+
+  
+  
+
+});
