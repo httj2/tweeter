78c4479b55aa3209b548ef17e7302113af95930f
Judith Huynh <judithuynh@gmail.com>
2020-04-24 04:07:38 +0000

    accidently made errors when branchign and some files got changed back to original, so had to go back and manually fix everything; everything is up to date, Need to fix media.css for responsive design

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
 
-
-// Using jQuery and an appropriate selector, register an event handler to the textarea element for the form inside of the .new-tweet section.
-
-// const textArea = document.getElementsById('tweet-text');
-// $("#btn").click(function() {
-//   var text = $('#tweet-text').val();
-//   console.log(text)
-//   })
-
-
   $('textarea#tweet-text').keyup(function(){
     maxLength = 140;
     currentLength = this.value.length
